85ef61b5f4b0821242856b1a87f425e3df12d86f
lilmortyj <781113402@qq.com>
2023-03-31 19:42:24 +0000

    change train rl to selfplay

--- examples/run_rl.py
+++ examples/run_rl.py
@@ -32,68 +32,79 @@ def train(args):
         }
     )
 
-    # Initialize the agent and use random agents as opponents
-    if args.algorithm == 'dqn':
-        from rlcard.agents import DQNAgent
-        agent = DQNAgent(
-            num_actions=env.num_actions,
-            state_shape=env.state_shape[0],
-            mlp_layers=[64,64],
-            device=device,
-        )
-    elif args.algorithm == 'nfsp':
-        from rlcard.agents import NFSPAgent
-        agent = NFSPAgent(
-            num_actions=env.num_actions,
-            state_shape=env.state_shape[0],
-            hidden_layers_sizes=[64,64],
-            q_mlp_layers=[64,64],
-            device=device,
-        )
-    agents = [agent]
-    for _ in range(1, env.num_players):
-        agents.append(RandomAgent(num_actions=env.num_actions))
+    agents = []
+    for _ in range(env.num_players):
+        # Initialize the agent and use random agents as opponents
+        if args.algorithm == 'dqn':
+            from rlcard.agents import DQNAgent
+            agent = DQNAgent(
+                num_actions=env.num_actions,
+                state_shape=env.state_shape[0],
+                mlp_layers=[64,64],
+                device=device,
+            )
+        elif args.algorithm == 'nfsp':
+            from rlcard.agents import NFSPAgent
+            agent = NFSPAgent(
+                num_actions=env.num_actions,
+                state_shape=env.state_shape[0],
+                hidden_layers_sizes=[64,64],
+                q_mlp_layers=[64,64],
+                device=device,
+            )
+        agents.append(agent)
+        
     env.set_agents(agents)
 
+    loggers = [Logger(os.path.join(args.log_dir, str(k))) for k in range(env.num_players)]
+    for i in range(env.num_players):
+        loggers[i] = loggers[i].__enter__()
+    
     # Start training
-    with Logger(args.log_dir) as logger:
-        for episode in range(args.num_episodes):
+    for episode in range(args.num_episodes):
 
-            if args.algorithm == 'nfsp':
-                agents[0].sample_episode_policy()
+        if args.algorithm == 'nfsp':
+            for agent_ in agents:
+                agent_.sample_episode_policy()
 
-            # Generate data from the environment
-            trajectories, payoffs = env.run(is_training=True)
+        # Generate data from the environment
+        trajectories, payoffs = env.run(is_training=True)
 
-            # Reorganaize the data to be state, action, reward, next_state, done
-            trajectories = reorganize(trajectories, payoffs)
+        # Reorganaize the data to be state, action, reward, next_state, done
+        trajectories = reorganize(trajectories, payoffs)
 
-            # Feed transitions into agent memory, and train the agent
-            # Here, we assume that DQN always plays the first position
-            # and the other players play randomly (if any)
-            for ts in trajectories[0]:
-                agent.feed(ts)
+        # Feed transitions into agent memory, and train the agent
+        # Here, we assume that DQN always plays the first position
+        # and the other players play randomly (if any)
+        for i in range(env.num_players):
+            for ts in trajectories[i]:
+                agents[i].feed(ts)
 
-            # Evaluate the performance. Play with random agents.
-            if episode % args.evaluate_every == 0:
-                logger.log_performance(
+        # Evaluate the performance. Play with random agents.
+        if episode % args.evaluate_every == 0:
+            result = tournament(
+                env,
+                args.num_eval_games,
+            )
+            for i in range(env.num_players):
+                loggers[i].log_performance(
                     episode,
-                    tournament(
-                        env,
-                        args.num_eval_games,
-                    )[0]
+                    result[i]
                 )
 
+    for i in range(env.num_players):
+        loggers[i].__exit__(None, None, None)
+        
         # Get the paths
-        csv_path, fig_path = logger.csv_path, logger.fig_path
+        csv_path, fig_path = loggers[i].csv_path, loggers[i].fig_path
 
-    # Plot the learning curve
-    plot_curve(csv_path, fig_path, args.algorithm)
+        # Plot the learning curve
+        plot_curve(csv_path, fig_path, args.algorithm)
 
-    # Save model
-    save_path = os.path.join(args.log_dir, 'model.pth')
-    torch.save(agent, save_path)
-    print('Model saved in', save_path)
+        # Save model
+        save_path = os.path.join(os.path.join(args.log_dir, str(i)), 'model.pth')
+        torch.save(agents[i], save_path)
+        print('Model saved in', save_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("DQN/NFSP example in RLCard")
@@ -137,7 +148,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--num_episodes',
         type=int,
-        default=10000,
+        default=500000,
     )
     parser.add_argument(
         '--num_eval_games',
@@ -147,7 +158,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--evaluate_every',
         type=int,
-        default=100,
+        default=1000,
     )
     parser.add_argument(
         '--log_dir',
